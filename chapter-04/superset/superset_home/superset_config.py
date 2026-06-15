9aec92c19e2bc59eb3bc2dfb05def679a4e50c3b
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-06-15 10:02:55 +0000

    setting up superset

--- chapter-04/superset/superset_home/superset_config.py
+++ chapter-04/superset/superset_home/superset_config.py
@@ -1,2 +1,8 @@
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://superset:superset@superset-db:5432/superset'
 SECRET_KEY = 'mysecretkey'
+WTF_CSRF_ENABLED = False
+ENABLE_PROXY_FIX = True
+PREFERRED_URL_SCHEME = 'https'
+SUPERSET_WEBSERVER_PROTOCOL = 'https'
+SUPERSET_WEBSERVER_ADDRESS = 'fantastic-umbrella-q6q5w799w7ph9w5q-8088.app.github.dev'
+SUPERSET_WEBSERVER_PORT = 443
